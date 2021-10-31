070f2201ae1311cdbc3431950e010596a4ef59d3
andrew <andrew@hubtel.com>
2021-10-31 10:20:39 +0000

    init push

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserProfile/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserProfile from "./components/UserProfile.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{ 
+    UserProfile
   }
+  
 }
 </script>
 
@@ -21,6 +21,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+
+body {
+  background: rgb(240, 248, 253);
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    @{{user.username}} - {{fullName}}
+    <b>Followers: {{this.followers}}</b>
+    <button @click="followUser" class="btn btn-primary pointer">Follow</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UserProfile',
+  data(){
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "dr.scrolls",
+        firstname: "Andrew",
+        lastname: "Nickson",
+        email: "dr.scrolls@gmail.com",
+        isAdmin: true
+      }
+    }
+  },
+  watch:{
+    followers(newFollowerCount, oldFollowerCount){
+      if(newFollowerCount > oldFollowerCount){
+        console.log(`${this.user.username} has gained a follower`);
+      }
+    }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstname} ${this.user.lastname}`;
+    }
+  },
+  methods: { 
+    followUser(){
+      this.followers++;
+    }
+  },
+  mounted(){
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
