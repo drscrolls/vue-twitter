010a078d07004af8a1dd422c200dd6d2c36de095
andrew <andrew@hubtel.com>
2021-11-03 23:52:51 +0000

    rm /dist from gitignore

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const serveStatic = require("serveStatic");
-const history = require("connect-history-api-fallback");
-const enforce = require("express-sslify");
+const express = require('express');
+const serveStatic = require('serveStatic');
+const history = require('connect-history-api-fallback');
+const enforce = require('express-sslify');
 
 const app = express();
 
 //use ssl (secure) version of website
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use('/', serveStatic(__dirname + ' / dist '));
 app.use(history());
 
 // app.listen(process.env.PORT || 5000);
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
