0f78669940d0573b0e3394692b351e272b4897b7
andrew <andrew@hubtel.com>
2021-11-03 22:13:47 +0000

    updated server listen port.

--- src/server.js
+++ src/server.js
@@ -11,4 +11,7 @@ app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + "/dist"));
 app.use(history());
 
-app.listen(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
