abb61dda204bec5d6abf46a40e5290714bd59b7c
andrew <andrew@hubtel.com>
2021-11-04 00:12:23 +0000

    reset code to tutorial

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const serveStatic = require("serveStatic");
+const serveStatic = require("serve-static");
 const history = require("connect-history-api-fallback");
 const enforce = require("express-sslify");
 
@@ -11,7 +11,4 @@ app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + "/dist"));
 app.use(history());
 
-// app.listen(process.env.PORT || 5000);
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 5000);
